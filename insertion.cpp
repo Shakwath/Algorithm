840476cfb0906a76ce3b0dd7aafeb91491ab6d7b
shakwath <shakaawathhossain0@gmail.com>
2025-02-24 18:12:06 +0600

    first commit

---
+++ insertion.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+// Function to perform insertion sort
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key
+        // one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    clock_t start = clock();
+    insertionSort(arr, n);
+    clock_t end = clock();
+
+    double time_taken = double(end - start) / CLOCKS_PER_SEC;
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+    cout << "Time taken: " << time_taken << " seconds" << endl;
+
+    return 0;
+}
